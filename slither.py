65f3761935bd34e5fd8e20c918ac8099726661c6
Said Ortigoza <a01707430@itesm.mx>
2021-09-17 13:12:26 -0500

    musica en slither agregada

--- slither.py
+++ slither.py
@@ -13,6 +13,21 @@ from random import random
 from random import randrange
 from turtle import *
 from freegames import square, vector
+from playsound import playsound
+from threading import Thread
+
+
+#Función para abrir un archivo de música
+def music_func():
+    playsound('musicSnake2.mid')
+
+
+#Definir función que llama audio
+music = Thread(target = music_func)
+music.daemon = True
+
+#Iniciar música
+music.start()
 
 food = vector(0, 0)
 snake = [vector(10, 0)]
